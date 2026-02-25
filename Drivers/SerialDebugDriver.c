31ac5ac240febad95e177b285c5d15c05da69a6e
Dan <danielshan824@gmail.com>
2026-02-25 00:12:06 -0500

    Squashed 'UOSM-Core/' content from commit 6065857
    git-subtree-dir: UOSM-Core
    git-subtree-split: 60658579b064421d37e5dbbf4a45df0eb5e0a152

---
+++ Drivers/SerialDebugDriver.c
@@ -0,0 +1,73 @@
+/*
+ * SerialDebugDriver.c
+ *
+ *  Created on: Sep. 5, 2022
+ *      Author: mingy
+ */
+
+#include "SerialDebugDriver.h"
+#ifdef SMALL_PRINTF
+#include "printf.h"
+#else
+#include <stdio.h>
+#endif
+
+#ifdef SMALL_PRINTF
+#include "printf.h"
+#else
+
+#include <stdio.h>
+
+#endif
+
+#ifdef STM
+static char messageBuf[MAX_SERIAL_PRINT_LENGTH];
+
+#ifdef DEBUG_UART
+extern UART_HandleTypeDef DEBUG_UART;
+#endif
+
+void SerialPrint(const char* message, ...) {
+#ifdef DEBUG_UART
+    va_list args;
+    va_start(args, message);
+#ifdef SMALL_PRINTF
+    uint16_t len = sprintf(messageBuf, message, args);
+#else
+    uint16_t len = vsprintf(messageBuf, message, args);
+#endif
+    HAL_UART_Transmit(&DEBUG_UART, (uint8_t*) messageBuf, len, HAL_MAX_DELAY);
+    va_end(args);
+#endif
+}
+
+void SerialPrintln(const char* message, ...) {
+#ifdef DEBUG_UART
+    va_list args;
+    va_start(args, message);
+#ifdef SMALL_PRINTF
+    uint16_t len = sprintf(messageBuf, message, args);
+#else
+    uint16_t len = vsprintf(messageBuf, message, args);
+#endif
+    messageBuf[len] = '\n';
+    messageBuf[len + 1] = '\r';
+    HAL_UART_Transmit(&DEBUG_UART, (uint8_t*) messageBuf, len + 2, HAL_MAX_DELAY);
+    va_end(args);
+#endif
+}
+
+#else
+void SerialPrint(const char * message, ...) {
+    va_list args;
+    va_start(args, message);
+    ExternalSerialPrint(message, args);
+    va_end(args);
+}
+void SerialPrintln(const char * message, ...) {
+    va_list args;
+    va_start(args, message);
+    ExternalSerialPrintln(message, args);
+    va_end(args);
+}
+#endif
